7ca50f0ec6088fb3a75e2456e5a86f716ea9624d
Onedelay <wjy0218@gmail.com>
2018-08-18 16:29:19 +0900

    MovieDao 재정의

--- app/src/main/java/com/onedelay/mymovie/database/MovieDao.java
+++ app/src/main/java/com/onedelay/mymovie/database/MovieDao.java
@@ -1,5 +1,6 @@
 package com.onedelay.mymovie.database;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -16,8 +17,11 @@ public interface MovieDao {
     void updateMovies(MovieEntity... movieEntities);
 
     @Query("SELECT * FROM Movies")
-    List<MovieEntity> selectMovies();
+    LiveData<List<MovieEntity>> selectMovies();
 
     @Query("SELECT * FROM Movies WHERE id = :id")
     MovieEntity selectMovieDetail(int id);
+
+    @Query("DELETE FROM Movies")
+    void clear();
 }
